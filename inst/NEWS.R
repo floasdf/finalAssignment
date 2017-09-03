691084c78e3c0047de19f9b6c62f0a2a28284b89
floasdf <flo.asdf@freenet.de>
2017-09-03 11:52:06 +0200

    <sdf

---
+++ inst/NEWS.R
@@ -0,0 +1,10 @@
+Package: finalAssignment
+================
+  
+  Version: 0.1.0 [2017-09-03]
+- Add cron_rscript
+- Add examples
+- Make sure Rscript uses full path and shQuote scripts
+- Roxygen updates
+- Vignette
+
